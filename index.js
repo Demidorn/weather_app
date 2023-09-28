0ba76087b60fd0aa526b5768bce3abb23b4e03e9
Demidorn <dornkaizen@gmail.com>
2023-09-28 20:44:40 +0300

    weather app

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello world!");
-console.log("Hello world!");
+const api_key ="81703a1a828fde0b3af47f4417415096"
+
+function fetchData(city) {
+   fetch("https://api.openweathermap.org/data/2.5/weather?q="
+    + city
+     +"&units=metric&APPID="+api_key
+     ).then((response) => response.json())
+     .then((data) => )
+}
+
