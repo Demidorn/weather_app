cefc7d5bd5708e5a769c1f827a8f9eb7bb96cbd5
Demidorn <dornkaizen@gmail.com>
2023-10-08 20:34:29 +0300

    Fetch from api

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const api_key ="81703a1a828fde0b3af47f4417415096"
+const inputinfo = document.querySelector(".search-data");
+const button = document.querySelector(".button");
 
+let api_key = "3a493f1d37e98aa9606b276ef24f470c";
 function fetchData(city) {
-   fetch("https://api.openweathermap.org/data/2.5/weather?q="
-    + city
-     +"&units=metric&APPID="+api_key
-     ).then((response) => response.json())
-     .then((data) => )
+   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+   city + "&units=metric&APPID=" + 
+   api_key)
+   .then((response) => {
+      if (!response.ok) {
+         throw new Error("Network is not ok");
+      }
+      return response.json();
+   })
+   .then((data) => {
+      console.log(data);
+   });
 }
 
+const search_data = () => {
+   const value = inputinfo.value;
+   return fetchData(value);
+};
+
+button.addEventListener("click", search_data);
